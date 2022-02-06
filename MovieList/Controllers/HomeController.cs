7593aa9cab17322cfd2f5eb047e6358bab6b5e91
kellwynn44 <63372677+kellwynn44@users.noreply.github.com>
2022-02-06 15:36:57 -0700

    Update HomeController.cs
    Removed redundant using statements.

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -1,22 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using MovieList.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MovieList.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
 
 namespace MovieList.Controllers
 {
